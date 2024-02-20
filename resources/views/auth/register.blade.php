c0ce0407ab17cffaed6a75046ea4e76e0b646274
VoidGravity <abdellahbardichwork@gmail.com>
2024-02-20 11:58:12 +0200

    semi fix for form submition

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Login</title>
 </head>
 
@@ -13,16 +14,23 @@
     <div class="login-page">
         <div class="form">
             <h1 class="">Register</h1>
-            
+
             <form class="login-form" method="post" action="{{ url('/register') }}">
-                @csrf
+                {{-- @csrf --}}
+                @php
+                    $myCsrf = csrf_token();
+                @endphp
+                <input type="hidden" name="_token" value="{{ $myCsrf }}">
+                @php
+                    session('_token', $myCsrf);
+                @endphp
                 <input name="username" type="text" placeholder="username" />
                 <input name="email" type="email" placeholder="email" />
                 <input name="address" type="text" placeholder="address" />
                 <input name="password" type="password" placeholder="password" />
                 <input name="password_confirmation" type="password" placeholder="Confirm Password" />
                 <button type="submit">Sigin in</button>
-                <p class="message">You have an acount? <a href="{{url('/login')}}">LogIn</a></p>
+                <p class="message">You have an acount? <a href="{{ url('/login') }}">LogIn</a></p>
             </form>
         </div>
     </div>
